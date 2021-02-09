af31c6a8c3f9e1efa12e6c3ce44d5596a813fe2c
basheer <basheer@hexagonsupport.com>
2021-02-09 19:02:15 +0530

    update forgot password page

--- src/app/user-registration/forgot-password/forgot-password.module.ts
+++ src/app/user-registration/forgot-password/forgot-password.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ForgotPasswordComponent } from './forgot-password.component';
+import { RouterModule , Routes } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+const routes: Routes = [
+  {path:'', component:ForgotPasswordComponent}
+]
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ForgotPasswordComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,RouterModule.forChild(routes),MatButtonModule,FormsModule,ReactiveFormsModule,MatFormFieldModule,MatInputModule,MatIconModule
+  ],
+  exports:[RouterModule]
 })
 export class ForgotPasswordModule { }
