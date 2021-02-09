3ef328f44b34561122dc30ca7481245c57573ea5
basheer <basheer@hexagonsupport.com>
2021-02-09 12:43:13 +0530

    signup page and welcome email template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
 
+const routes: Routes = [
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
+{path: 'users', loadChildren: () => import('./user-registration/user-registration.module').then(m => m.UserRegistrationModule)}
+];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot(routes),FormsModule,ReactiveFormsModule,
+    BrowserAnimationsModule,MatButtonModule,MatFormFieldModule,MatInputModule,MatIconModule
   ],
   providers: [],
+  exports:[RouterModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
