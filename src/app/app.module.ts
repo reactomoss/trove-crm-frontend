ea15273b21b2b899ea08da52ab9d0c5e4fecde26
basheer <basheer@hexagonsupport.com>
2021-02-11 00:33:06 +0530

    update user module pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
-
 const routes: Routes = [
   { path: '', redirectTo: 'user', pathMatch: 'full' },
 {path: 'user', loadChildren: () => import('./user-registration/user-registration.module').then(m => m.UserRegistrationModule)}
--- src/app/user-registration/forgot-password/forgot-password.module.ts
+++ src/app/user-registration/forgot-password/forgot-password.module.ts
@@ -7,15 +7,20 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
+import {CountdownModule} from 'ngx-countdown';
+import { ResetEmailComponent } from './reset-email/reset-email.component';
+import { NewPasswordComponent } from './new-password/new-password.component';
 const routes: Routes = [
-  {path:'', component:ForgotPasswordComponent}
+  {path:'', component:ForgotPasswordComponent},
+  {path:'reset-email', component:ResetEmailComponent},
+  {path:'new-password', component:NewPasswordComponent}
 ]
 
 
 @NgModule({
-  declarations: [ForgotPasswordComponent],
+  declarations: [ForgotPasswordComponent, ResetEmailComponent, NewPasswordComponent],
   imports: [
-    CommonModule,RouterModule.forChild(routes),MatButtonModule,FormsModule,ReactiveFormsModule,MatFormFieldModule,MatInputModule,MatIconModule
+    CommonModule,RouterModule.forChild(routes),MatButtonModule,FormsModule,ReactiveFormsModule,MatFormFieldModule,MatInputModule,MatIconModule,CountdownModule
   ],
   exports:[RouterModule]
 })
