1b293998203df84911d1c2ade35f1d785e499b62
basheer <basheer@hexagonsupport.com>
2021-02-19 15:28:43 +0530

    update header section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,13 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {SharedModule} from './shared/shared.module';
 const routes: Routes = [
   { path: '', redirectTo: 'user', pathMatch: 'full' },
-{path: 'user', loadChildren: () => import('./user-registration/user-registration.module').then(m => m.UserRegistrationModule)}
+{path: 'user', loadChildren: () => import('./user-registration/user-registration.module').then(m => m.UserRegistrationModule)},
+{path:'pages' , loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)},
+{path: '' , loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule)}
 ];
 @NgModule({
   declarations: [
@@ -18,7 +22,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,RouterModule.forRoot(routes),FormsModule,ReactiveFormsModule,
-    BrowserAnimationsModule,MatButtonModule,MatFormFieldModule,MatInputModule,MatIconModule
+    BrowserAnimationsModule,MatButtonModule,MatFormFieldModule,MatInputModule,MatIconModule,SharedModule,MatMenuModule
   ],
   providers: [],
   exports:[RouterModule],
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
-
+import { PagesComponent } from './pages.component';
+import { RouterModule ,  Routes } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
+import { SettingsComponent } from './settings/settings.component';
+import { MatTabsModule } from "@angular/material/tabs";
+const routes : Routes = [
+  {path:'' , redirectTo: 'dashboard' , pathMatch:'full'},
+{path: 'dashboard' , component:PagesComponent},
+{path:'settings' , component:SettingsComponent}
+]
 @NgModule({
-  declarations: [],
+  declarations: [PagesComponent , SettingsComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule, RouterModule.forChild(routes), SharedModule, MatTabsModule
+  ],
+  exports:[RouterModule],
 })
 export class PagesModule { }
