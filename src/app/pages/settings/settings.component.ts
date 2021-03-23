378a5d0732ea915cde48ea8bed58319719e7f335
basheer <basheer@hexagonsupport.com>
2021-03-23 14:14:43 +0530

    update style.css and settings.component.ts

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -1,35 +1,14 @@
 import {Component, OnInit} from '@angular/core';
-// import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
-// import { FormGroup, FormControl,FormBuilder, Validators} from '@angular/forms';
-// import {MatTableDataSource} from '@angular/material/table';
-// import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
-// import {SnackBarService} from '../../shared/snack-bar.service';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.css'],
 })
 export class SettingsComponent implements OnInit {
-
-/*Modal dialog*/
-closeResult = '';
-/*Modal dialog*/
   constructor() {
   }
-
-  //defining method for display of SnackBar
-  // triggerSnackBar(message:string, action:string)
-  // {
-  //  this.sb.openSnackBarBottomCenter(message, action);
-  // }
-
-
-
-
 
   ngOnInit(): void {
 
   }
 }
-
-
